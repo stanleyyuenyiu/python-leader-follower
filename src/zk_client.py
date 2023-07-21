3a3a57ce029173a43ffdf1d32c277eeede1cf652
stanleyyuen <ypang.yuen@gmail.com>
2023-07-21 15:15:20 +0800

    chore: chg leader election method
    - add watcher event to set leader

--- src/zk_client.py
+++ src/zk_client.py
@@ -2,7 +2,7 @@ from kazoo.client import KazooClient
 from kazoo.client import KazooState
 import logging
 from interface import IZkClient
-
+from datetime import datetime
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger('zookepper')
@@ -11,6 +11,8 @@ logger.setLevel('DEBUG')
 class ZkClient(IZkClient):
     def __init__(self, hosts) -> None:
         self.zk = KazooClient(hosts=hosts)
+        self.server_id = None
+        self._is_leader = False
         self.node_path = []
 
     def _state_listener(self, state):
@@ -23,32 +25,35 @@ class ZkClient(IZkClient):
 
     def start(self, node_path = ['/node', 'leader']):
         self.node_path = node_path
-
         self.zk.start()
-        self.curr_node = self._create_node(self.node_path)
-        
+        self._register(self.node_path)
+        self._watch_application_nodes()
+        self._watch_state()
+
+    def _watch_state(self):
         self.zk.add_listener(self._state_listener)
 
-    def _watcher(self, *args, **kwargs):
-        logger.debug("Data changed: %s" % (args[0]))
+    def _watch_application_nodes(self):
+        self.zk.ChildrenWatch(path='/node', func=self._election)
+        
+    def _election(self, children):
+        children.sort()
+        server_id = self.server_id.split('/')[-1]
+        if children[0] == server_id:
+            self._is_leader = True
+            logger.debug('Instance is the leader')
+        else:
+            self._is_leader = False
+            logger.debug('Instance is the follower')
 
-    def _create_node(self, path = []):
+    def _register(self, path = []):
         if not path:
             raise Exception('path is empty')
         self.zk.ensure_path(path[0])
-        return self.zk.create('/'.join(path), value=b'data', ephemeral=True, sequence=True)
+        self.server_id = self.zk.create('/'.join(path), value=b'data', ephemeral=True, sequence=True)
 
     def is_leader(self):
-        nodes = self.zk.get_children(self.node_path[0])
-        # Sort the nodes by their sequence number
-        nodes.sort()
-        # If this instance is the first node in the list, it becomes the leader
-        if self.curr_node == self.node_path[0] + '/' + nodes[0]:
-            logger.debug('Instance is the leader')
-            return True
-        else:
-            logger.debug('Instance is a follower')
-            return False
+        return self._is_leader
         
     def close(self):
         self.zk.stop()
